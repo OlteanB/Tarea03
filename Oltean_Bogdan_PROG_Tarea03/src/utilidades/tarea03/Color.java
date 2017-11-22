ee9659a75c5abbdeb0e034837de4a1416c512fdc
Bogdan <bogdan>
2017-11-22 13:59:22 +0100

    Commit del enumerado Color

---
+++ Oltean_Bogdan_PROG_Tarea03/src/utilidades/tarea03/Color.java
@@ -0,0 +1,11 @@
+
+package utilidades.tarea03;
+
+/**
+ * Enumerado que contiene los valores que puede tomar el rey de ajedrez
+ * 
+ * @author bogdan
+ */
+public enum Color {
+    BLANCO, NEGRO;
+}
