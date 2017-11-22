870487a31145c0457d9a255c52c41746361b676d
Bogdan <bogdan>
2017-11-22 14:04:03 +0100

    Commit del enumerado Direccion

---
+++ Oltean_Bogdan_PROG_Tarea03/src/utilidades/tarea03/Direccion.java
@@ -0,0 +1,11 @@
+
+package utilidades.tarea03;
+
+/**
+ * Tipo de dato enumerado que contiene los movimientos que puede tomar el rey en la tabla de ajedrez
+ * 
+ * @author bogdan
+ */
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE;
+}
